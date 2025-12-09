07f138edcd63358d3d144e0ee47783d81a81ba0e
Alexander <AlexanderKemosCA@gmail.com>
2025-12-08 17:44:36 -0800

    Non-recursive binary_search.py addeD

--- binary_search.py
+++ binary_search.py
@@ -1,18 +1,17 @@
 def binary_search(arr: list[int], target: int) -> int:
-    # WRITE YOUR BRILLIANT CODE HERE
-
-    left = 0, right = len(arr)-1 # Right points to last and left to first element.
-    mid = (left + right)//2
-
+    """Binary Search non-recursive"""
+    
+    left = 0
+    right = len(arr)-1
     # While array is not exhausted
     while( right >= left):
-        mid = (left + right)//2 # Middle index of array (round down)
+        mid = (left + right)//2
         if target == arr[mid]:
             return mid
         elif target > arr[mid]:
-            left = mid
+            left = mid + 1
         else:
-            right = mid
+            right = mid - 1
     return -1
 
 if __name__ == "__main__":
